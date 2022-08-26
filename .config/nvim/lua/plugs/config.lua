44ac209a6b43a2d5b4b5d10cb5e2c9dc9418439b
Lucas Burns <burnsac@me.com>
2022-08-26 12:11:49 -0500

    update(nvim): trevj for teal

--- .config/nvim/lua/plugs/config.lua
+++ .config/nvim/lua/plugs/config.lua
@@ -1749,9 +1749,9 @@ function M.trevj()
                     parameters = {final_separator = false, final_end_line = true}
                 },
                 teal = {
-                    -- table_constructor = {final_separator = ",", final_end_line = true},
+                    table_constructor = {final_separator = ",", final_end_line = true},
                     arguments = {final_separator = false, final_end_line = true},
-                    -- parameters = {final_separator = false, final_end_line = true}
+                    parameters = {final_separator = false, final_end_line = true}
                 },
                 html = {
                     start_tag = {
